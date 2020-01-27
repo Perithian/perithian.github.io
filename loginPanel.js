ad2d3ae26522f3400779269e744a32877ebea71a
Perithian <50120491+Perithian@users.noreply.github.com>
2020-01-27 13:46:25 +0000

    added more firebase functionality

--- loginPanel.js
+++ loginPanel.js
@@ -1,3 +1,11 @@
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+  } else {
+    // No user is signed in.
+  }
+});
+
 function login(){
   
   var userEmail = document.getElementById("email_field").value;
